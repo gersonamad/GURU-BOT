cebde8ff80b7442467d57fa4a605622adaf0db45
Gerson coder <88016026+GersonAmade@users.noreply.github.com>
2023-07-13 17:32:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
+  ['258876442534', 'Gidion bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
